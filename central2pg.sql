b8c34e1c2a267240b64e5969bce58b69e9a6aa5f
mathieubossaert <mathieu.bossaert@cenlr.org>
2021-05-19 13:08:13 +0200

    closes #6 use search_path to avoid schema reference missing into function calls

--- central2pg.sql
+++ central2pg.sql
@@ -1,6 +1,7 @@
 /*
    change schema name odk_central
    on two firts lines to what you want
+Do the same for "SET search_path=odk_central;" statement in the feed_data_tables_from_central(text,text) function
 */
 CREATE SCHEMA IF NOT EXISTS odk_central;
 SET SEARCH_PATH TO odk_central;
@@ -303,6 +304,9 @@ COMMENT ON function create_table_from_refcursor(text,text,refcursor) IS 'descrip
 
 /*
 FUNCTION: feed_data_tables_from_central(text, text)
+
+!!! You need to edit and set the correct search_path at the beginig of the EXECUTE statement : SET search_path=odk_central; Replace "odk_central" by the name of the schema where you created the functions !!!
+
 	description : 
 		Feed the tables from key/pair tables. 
 	parameters :
@@ -326,12 +330,12 @@ CREATE OR REPLACE FUNCTION feed_data_tables_from_central(
     COST 100
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
-declare requete_a text;
-declare requete_b text;
-declare requete_c text;
+declare query_a text;
+declare query_b text;
+declare query_c text;
 declare columns_list text;
 BEGIN
-EXECUTE format('
+EXECUTE format('SET search_path=odk_central;
 	DROP TABLE IF EXISTS data_table;
 	CREATE TABLE data_table(data_id text, key text, value json);
 	INSERT INTO  data_table(data_id, key, value) 
@@ -354,14 +358,14 @@ EXECUTE format('
 	)SELECT data_id, key, value FROM doc_key_and_value_recursive WHERE json_typeof(value) <> ''object'' OR key IN (''point'',''ligne'',''polygone'') ORDER BY 2,1;'
 );
 
-requete_a = 'SELECT data_id, key, value FROM data_table ORDER BY 1,2';
-requete_b = 'SELECT DISTINCT key FROM data_table ORDER BY 1';
-requete_c = concat('SELECT dynamic_pivot(''',requete_a,''',''', requete_b,''',''curseur_central'');
+query_a = 'SELECT data_id, key, value FROM data_table ORDER BY 1,2';
+query_b = 'SELECT DISTINCT key FROM data_table ORDER BY 1';
+query_c = concat('SELECT dynamic_pivot(''',query_a,''',''', query_b,''',''curseur_central'');
 			   		SELECT create_table_from_refcursor(''',schema_name,''',''',table_name,'_data'', ''curseur_central'');
 			   		MOVE BACKWARD FROM "curseur_central";
 			   		SELECT insert_into_from_refcursor(''',schema_name,''',''',table_name,'_data'', ''curseur_central'');
 				   	CLOSE "curseur_central"');
-EXECUTE (requete_c);
+EXECUTE (query_c);
 END;
 $BODY$;
 
--- feed_data_tables_from_central.sql
+++ feed_data_tables_from_central.sql
@@ -1,5 +1,8 @@
 /*
 FUNCTION: feed_data_tables_from_central(text, text)
+
+!!! You need to edit and set the correct search_path at the beginig of the EXECUTE statement : SET search_path=odk_central; Replace "odk_central" by the name of the schema where you created the functions !!!
+
 	description : 
 		Feed the tables from key/pair tables. 
 	parameters :
@@ -23,12 +26,12 @@ CREATE OR REPLACE FUNCTION feed_data_tables_from_central(
     COST 100
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
-declare requete_a text;
-declare requete_b text;
-declare requete_c text;
+declare query_a text;
+declare query_b text;
+declare query_c text;
 declare columns_list text;
 BEGIN
-EXECUTE format('
+EXECUTE format('SET search_path=odk_central;
 	DROP TABLE IF EXISTS data_table;
 	CREATE TABLE data_table(data_id text, key text, value json);
 	INSERT INTO  data_table(data_id, key, value) 
@@ -51,14 +54,14 @@ EXECUTE format('
 	)SELECT data_id, key, value FROM doc_key_and_value_recursive WHERE json_typeof(value) <> ''object'' OR key IN (''point'',''ligne'',''polygone'') ORDER BY 2,1;'
 );
 
-requete_a = 'SELECT data_id, key, value FROM data_table ORDER BY 1,2';
-requete_b = 'SELECT DISTINCT key FROM data_table ORDER BY 1';
-requete_c = concat('SELECT dynamic_pivot(''',requete_a,''',''', requete_b,''',''curseur_central'');
+query_a = 'SELECT data_id, key, value FROM data_table ORDER BY 1,2';
+query_b = 'SELECT DISTINCT key FROM data_table ORDER BY 1';
+query_c = concat('SELECT dynamic_pivot(''',query_a,''',''', query_b,''',''curseur_central'');
 			   		SELECT create_table_from_refcursor(''',schema_name,''',''',table_name,'_data'', ''curseur_central'');
 			   		MOVE BACKWARD FROM "curseur_central";
 			   		SELECT insert_into_from_refcursor(''',schema_name,''',''',table_name,'_data'', ''curseur_central'');
 				   	CLOSE "curseur_central"');
-EXECUTE (requete_c);
+EXECUTE (query_c);
 END;
 $BODY$;
 
