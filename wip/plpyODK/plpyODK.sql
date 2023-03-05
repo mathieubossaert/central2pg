2a586de02f4b149c0c531eb9ab5513dd37a2a8f0
mathieubossaert <mathieu.bossaert@cenlr.org>
2023-03-05 23:11:54 +0100

    add get_filtered_datas functions that returs fresh datas only

--- wip/plpyODK/plpyODK.sql
+++ wip/plpyODK/plpyODK.sql
@@ -1,65 +1,48 @@
-/* Récupération des submissions_id filtrés */
-/* sous la forme "Submissions('uuid%3Adf981ea2-8c28-4133-9bfe-06c02c75522e')" pour utilise rensuite une fonction générique */
-
-CREATE OR REPLACE FUNCTION plpyodk.client_get_filtered_submissions_ids(
+CREATE OR REPLACE FUNCTION plpyodk.get_filtered_datas(
 	param_project_id text,
 	param_form_id text,
-	filter text)
-    RETURNS text[]
+	param_criteria text
+)
+    RETURNS text
     LANGUAGE 'plpython3u'
     COST 100
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
-	from pyodk.client import Client
-	client = Client()
-	client.open()
-	response = client.get("projects/"+param_project_id+"/forms/"+param_form_id+".svc/Submissions?$filter=__system/"+filter)
-	submissions = []
-	for s in (response.json()['value']):
-		submissions.append("Submissions('"+s['__id']+"')")
-	return submissions
-	
-$BODY$;
+def fresh_data_only(project_id, form_id, path, criteria,  datas):
+    from pyodk.client import Client
+    import re
+    import json 
+    client = Client()
+    client.open()
+    if path == '':
+        return None
 
+    url = 'projects/'+project_id+'/forms/'+form_id+'.svc/'+path+'?$filter='+criteria
+    if re.match(r"Submissions\?.*", path) or re.match(r".*\)$", path):
+        tablename = 'submissions'
+    else:
+        tablename = path.rsplit('/')[-1]    
+    
+    response = client.get(url)
+    
+    value = response.json()['value']
+    
+    navigationlinks = re.findall(r'(\'\w+@odata\.navigationLink\'):\s+([^\}]+)', str(value))
+    for (key, link) in navigationlinks:
+        link = link.replace("'", "'").replace('"','')
+        fresh_data_only(project_id, form_id, link, '', datas)
+    
+    if tablename in datas.keys():
+        datas[tablename] += value
+    else:
+        datas[tablename]=value
+		
+    json_datas = json.dumps(datas, indent = 4) 
+    return json_datas
+	
+return fresh_data_only(param_project_id, param_form_id, 'Submissions', param_criteria, datas = {})
+
+$BODY$;
 /*
-SELECT array_to_string(plpyodk.client_get_filtered_submissions_ids(
-	'5',
-	'Sicen_2022',
-	'submissionDate ge 2022-12-01'),',');
-
-retourne : Submissions('uuid:8700f252-bd7a-4373-909a-6eba210bd56c'),Submissions('uuid:df981ea2-8c28-4133-9bfe-06c02c75522e'),Submissions('uuid:f78250e7-ff0b-4537-ac54-0042759a27cc')
+SELECT * from json_each(plpyodk.get_filtered_datas('5', 'Sicen_2022','__system/submissionDate ge 2023-03-05')::json)
 */
-
-
-
-CREATE OR REPLACE FUNCTION plpyodk.client_get_table_by_path(
-	param_project_id text,
-	param_form_id text,
-	paths text)
-    RETURNS json
-    LANGUAGE 'plpython3u'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-AS $BODY$
-	from pyodk.client import Client
-	import json
-	client = Client()
-	client.open()
-	data = []
-	paths_list = paths.split(",")
-	for path in paths_list:
-		response = client.get("projects/"+param_project_id+"/forms/"+param_form_id+".svc/"+path)
-		data.append(response.json())
-
-	return(json.dumps(data))
-
-$BODY$;
-
-SELECT plpyodk.client_get_table_by_path(
-	'5',
-	'Sicen_2022',
-    (SELECT array_to_string(plpyodk.client_get_filtered_submissions_ids(
-	'5',
-	'Sicen_2022',
-	'submissionDate ge 2022-12-01'),',')))
-	
