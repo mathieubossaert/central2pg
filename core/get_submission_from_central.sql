17c67008cdcffeee0d240dcfd8eae283c7cd73ec
mathieubossaert <mathieu.bossaert@cenlr.org>
2023-09-26 11:52:57 +0200

    use last_submission_date as a filter

--- core/get_submission_from_central.sql
+++ core/get_submission_from_central.sql
@@ -1,7 +1,7 @@
 
 
 /*
-FUNCTION: get_submission_from_central(text, text, text, integer, text, text, text, text, text, text, text)
+FUNCTION: get_submission_from_central(text, text, text, integer, text, text, text, text)
 	description
 		Get json data from Central, feed a temporary table with a generic name central_json_from_central.
 		Once the temp table is created and filled, PG checks if the destination schema and (permanent) table exist. If not PG creates it with only one json column named "value".
@@ -83,3 +83,88 @@ COMMENT ON FUNCTION  odk_central.get_submission_from_central(text,text,text,inte
 
 	comment : 	
 	future version should use filters... With more parameters';
+	
+
+
+/*
+FUNCTION: get_submission_from_central(text, text, text, integer, text, text, text, text, text)
+	description
+		Get json data from Central, feed a temporary table with a generic name central_json_from_central.
+		Once the temp table is created and filled, PG checks if the destination schema and (permanent) table exist. If not PG creates it with only one json column named "value".
+		PG does the same to check if a unique constraint on the __id exists. This index will be use to ignore subissions already previously inserted in the table, using an "ON CONFLICT xxx DO NOTHING"
+	
+	parameters :
+		email text						-- the login (email adress) of a user who can get submissions
+		password text					-- his password
+		central_domain text 			-- ODK Central fqdn : central.mydomain.org
+		project_id integer				-- the Id of the project ex. 4
+		form_id text					-- the name of the Form ex. Sicen
+		form_table_name text			-- the table of the form to get value from (one of thoses returned by get_form_tables_list_from_central() function
+		destination_schema_name text 	-- the name of the schema where to create the permanent table 
+		destination_table_name text		-- the name of this table 
+		last_submission_timestamp		-- the timstamp to use as a filter (sumissions with submission date greater or equal to this parameter) 
+	
+	returning :
+		void
+
+	comment : 	
+	future version should use filters... With more parameters
+*/
+
+CREATE OR REPLACE FUNCTION odk_central.get_submission_from_central(
+	email text,						
+	password text,					
+	central_domain text, 			
+	project_id integer,				
+	form_id text,					
+	form_table_name text,			
+	destination_schema_name text, 	
+	destination_table_name text, 
+	last_submission_timestamp text
+	)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+declare url text;
+declare requete text;
+BEGIN
+url = replace(replace(concat('https://',central_domain,'/v1/projects/',project_id,'/forms/',form_id,'.svc/',form_table_name,'?$filter=$root/Submissions/__system/submissionDate ge ',last_submission_timestamp),' ','%%20'),'$','%%24');
+EXECUTE (
+		'DROP TABLE IF EXISTS central_json_from_central;
+		 CREATE TEMP TABLE central_json_from_central(form_data json);'
+		);
+
+EXECUTE format('COPY central_json_from_central FROM PROGRAM $$ curl --insecure --max-time 30 --retry 5 --retry-delay 0 --retry-max-time 40 -X GET "'||url||'" -H "Accept: application/json" -H ''Authorization: Bearer '||odk_central.get_token_from_central(email, password, central_domain)||''' $$ CSV QUOTE E''\x01'' DELIMITER E''\x02'';');
+
+EXECUTE format('CREATE SCHEMA IF NOT EXISTS '||destination_schema_name||';
+CREATE TABLE IF NOT EXISTS '||destination_schema_name||'.'||destination_table_name||' (form_data json);');
+	IF odk_central.does_index_exists(destination_schema_name,destination_table_name) IS FALSE THEN
+		EXECUTE format ('CREATE UNIQUE INDEX IF NOT EXISTS idx_'||left(md5(random()::text),20)||'
+		ON '||destination_schema_name||'.'||destination_table_name||' USING btree
+		((form_data ->> ''__id''::text) COLLATE pg_catalog."default" ASC NULLS LAST)
+		TABLESPACE pg_default;');
+	END IF;	
+EXECUTE format('TRUNCATE '||destination_schema_name||'.'||destination_table_name||'; INSERT into '||destination_schema_name||'.'||destination_table_name||'(form_data) SELECT json_array_elements(form_data -> ''value'') AS form_data FROM central_json_from_central ON CONFLICT ((form_data ->> ''__id''::text)) DO NOTHING;');
+END;
+$BODY$;
+
+COMMENT ON FUNCTION  odk_central.get_submission_from_central(text,text,text,integer,text,text,text,text,text)
+	IS 'description :
+		Get json data from Central, feed a temporary table with a generic name central_json_from_central.
+		Once the temp table is created and filled, PG checks if the destination schema and (permanent) table exists. If not PG creates it with only one json column named "value".
+		PG does the same to check if a unique constraint on the __id exists. This index will be use to ignore subissions already previously inserted in the table, using an "ON CONFLICT xxx DO NOTHING"
+	
+	parameters :
+		email text						-- the login (email adress) of a user who can get submissions
+		password text					-- his password
+		central_domain text 			-- ODK Central fqdn : central.mydomain.org
+		project_id integer				-- the Id of the project ex. 4
+		form_id text					-- the name of the Form ex. Sicen
+		form_table_name text			-- the table of the form to get value from (one of thoses returned by get_form_tables_list_from_central() function
+		destination_schema_name text 	-- the name of the schema where to create the permanent table 
+		destination_table_name text		-- the name of this table 
+		last_submission_timestamp		-- the timstamp to use as a filter (sumissions with submission date greater or equal to this parameter) 
+	
+	returning : void';
--- core/odk_central_to_pg.sql
+++ core/odk_central_to_pg.sql
@@ -67,3 +67,76 @@ COMMENT ON FUNCTION odk_central.odk_central_to_pg(text, text, text, integer, tex
 	
 	returning :
 		void';
+		
+
+
+/*
+FUNCTION: odk_central_to_pg(text, text, text, integer, text, text)
+
+	description :
+		Wraps the calling of both get_submission_from_central() and feed_data_tables_from_central() functions 
+		
+	parameters :
+		email text						-- the login (email adress) of a user who can get submissions
+		password text					-- his password
+		central_domain text 			-- ODK Central FQDN : central.mydomain.org
+		project_id integer				-- the Id of the project ex. 4
+		form_id text					-- the name of the Form ex. Sicen
+		destination_schema_name text 	-- the name of the schema where to create the permanent table 
+		geojson_columns text 			-- geojson colmuns to ignore in recursion, comma delimited list like ''geopoint_widget_placementmap,point,ligne,polygone'', depending on your question names. /!\ Beware of spaces ! /!\
+	
+	returning :
+		void
+*/
+
+CREATE OR REPLACE FUNCTION odk_central.odk_central_to_pg(
+	email text,
+	password text,
+	central_domain text,
+	project_id integer,
+	form_id text,
+	destination_schema_name text,
+	geojson_columns text,
+	last_submission_timestamp text)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+BEGIN
+EXECUTE format('SELECT odk_central.get_submission_from_central(
+	user_name,
+	pass_word,
+	central_FQDN,
+	project,
+	form,
+	tablename,
+	'''||destination_schema_name||''',
+	lower(trim(regexp_replace(left(concat(''form_'',form,''_'',split_part(tablename,''.'',cardinality(regexp_split_to_array(tablename,''\.'')))),58), ''[^a-zA-Z\d_]'', ''_'', ''g''),''_''))
+	,'''||last_submission_timestamp||'''
+	)
+FROM odk_central.get_form_tables_list_from_central('''||email||''','''||password||''','''||central_domain||''','||project_id||','''||form_id||''');');
+
+EXECUTE format('
+SELECT odk_central.feed_data_tables_from_central(
+	'''||destination_schema_name||''',lower(trim(regexp_replace(left(concat(''form_'',form,''_'',split_part(tablename,''.'',cardinality(regexp_split_to_array(tablename,''\.'')))),58), ''[^a-zA-Z\d_]'', ''_'', ''g''),''_'')),'''||geojson_columns||''')
+FROM odk_central.get_form_tables_list_from_central('''||email||''','''||password||''','''||central_domain||''','||project_id||','''||form_id||''');');
+END;
+$BODY$;
+
+COMMENT ON FUNCTION odk_central.odk_central_to_pg(text, text, text, integer, text, text, text, text)
+    IS 'description :
+		wraps the calling of both get_submission_from_central() and feed_data_tables_from_central() functions 
+		
+	parameters :
+		email text						-- the login (email adress) of a user who can get submissions
+		password text					-- his password
+		central_domain text 			-- ODK Central FQDN : central.mydomain.org
+		project_id integer				-- the Id of the project ex. 4
+		form_id text					-- the name of the Form ex. Sicen
+		destination_schema_name text 	-- the name of the schema where to create the permanent table 
+		geojson_columns text 			-- geojson colmuns to ignore in recursion, comma delimited list like ''geopoint_widget_placementmap,point,ligne,polygone'', depending on your question names. /!\ Beware of spaces ! /!\ 
+		last_submission_timestamp		-- the timstamp to use as a filter (sumissions with submission date greater or equal to this parameter) 
+	
+	returning :
+		void';
